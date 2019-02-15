40ec15386160acf912939881b7aa19128b792211
Bob Gong <gongyuan94@gmail.com>
2019-02-15 13:07:59 +0800

    deploy create react app built web pages

---
+++ docs/precache-manifest.5d43cd7e2ac892334eca5b6b5e899a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poker-guandan-strategy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "860f92ab8a4a13986135",
+    "url": "/poker-guandan-strategy/static/js/runtime~main.860f92ab.js"
+  },
+  {
+    "revision": "26d73fd8515b87880e35",
+    "url": "/poker-guandan-strategy/static/js/main.26d73fd8.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/poker-guandan-strategy/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "26d73fd8515b87880e35",
+    "url": "/poker-guandan-strategy/static/css/main.91ef6be1.chunk.css"
+  },
+  {
+    "revision": "cbb7a3d77a03363fcbfa5031b9d41311",
+    "url": "/poker-guandan-strategy/index.html"
+  }
+];
