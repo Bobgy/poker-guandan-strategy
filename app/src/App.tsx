84afb5fade2b9176c0be848928876491be4b1a45
Bob Gong <gongyuan94@gmail.com>
2019-02-18 15:43:09 +0800

    integrate with strategy module

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,56 @@
 /// <reference path="lib.d.ts"/>
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Button } from 'react-native'
 import { createSwitchNavigator } from '@react-navigation/core'
 import { createBrowserApp } from '@react-navigation/web'
-import { NavigationProps } from './types'
+import { NavigationProps, TCard } from './types'
 import { CardsChooser } from './CardsChooser'
 import { RankChooser, useCardState } from './useCardState'
+import { loadCppModule, PortedCppModule, StrategyResult } from './loadCppModule'
+
+let strategyModule: PortedCppModule | null = null
+
+function cardsToString(cards: TCard[]) {
+  return cards
+    .map(card => {
+      if (card.rank === 'X') {
+        // big joker and small joker
+        return card.suit === 'R' ? 'BJ' : 'SJ'
+      }
+      return card.rank + card.suit
+    })
+    .join('')
+}
 
 function Home({ screenProps, navigation }: NavigationProps) {
   const { rank, setRank, cards, clearCards, addCard } = screenProps
+  const [strategyResult, setResult]: [
+    null | StrategyResult,
+    (result: StrategyResult) => void
+  ] = useState(null) as any
 
   return (
     <View>
       <RankChooser rank={rank} setRank={setRank} />
       <CardsChooser cards={cards} addCard={addCard} clearCards={clearCards} />
       <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        title="计算策略"
+        onPress={() => {
+          if (strategyModule != null) {
+            const cardsStr = cardsToString(cards)
+            console.log(cardsStr)
+
+            setResult(strategyModule.calc(cardsStr, rank))
+          }
+        }}
       />
+      {strategyResult && (
+        <View>
+          <Text>{`最少${strategyResult.minHands}手可以出完`}</Text>
+          <Text>{strategyResult.solutions.join('\n')}</Text>
+        </View>
+      )}
     </View>
   )
 }
@@ -51,6 +83,18 @@ const AppNavigator = createSwitchNavigator(
 function App(props: any) {
   const [rank, setRank] = useState('2')
   const cardStateProps = useCardState()
+  useEffect(() => {
+    loadCppModule().then(cppModule => {
+      strategyModule = cppModule
+      // uncomment the following to test the module
+      // console.log(
+      //   strategyModule.calc(
+      //     'BJADKDJH0S0C0CAH8D7S7H4H4D4H3D3C2H9C7CAH6C5CKSQSJS0S9S',
+      //     'A',
+      //   ),
+      // )
+    })
+  }, [])
 
   return (
     <View
--- app/src/loadCppModule.ts
+++ app/src/loadCppModule.ts
@@ -1,67 +1,70 @@
 interface VectorString {
-    size: () => number;
-    get: (index: number) => string;
+  size: () => number
+  get: (index: number) => string
 }
 
 interface OriginalStrategyResult {
-    minHands: number,
-    solutions: VectorString,
+  minHands: number
+  solutions: VectorString
 }
 
-interface StrategyResult {
-    minHands: number,
-    solutions: string[],
+export interface StrategyResult {
+  minHands: number
+  solutions: string[]
 }
 
 interface CppModule {
-    onRuntimeInitialized: () => void,
-    onAbort: () => void,
-    add: (a: number, b: number) => number,
-    calc: (cards: string, mainRank: number) => OriginalStrategyResult,
+  onRuntimeInitialized: () => void
+  onAbort: () => void
+  add: (a: number, b: number) => number
+  calc: (cards: string, mainRank: number) => OriginalStrategyResult
 }
 
-interface PortedCppModule {
-    calc: (cards: string, mainRank: string) => StrategyResult,
+export interface PortedCppModule {
+  calc: (cards: string, mainRank: string) => StrategyResult
 }
 
 declare global {
-    interface Window {
-        Module: CppModule,
-    }
+  interface Window {
+    Module: CppModule
+  }
 }
 
 function vector2Array(vec: VectorString): string[] {
-    const arr = [];
-    for (let i = 0; i < vec.size(); ++i) {
-        arr.push(vec.get(i));
-    }
+  const arr = []
+  for (let i = 0; i < vec.size(); ++i) {
+    arr.push(vec.get(i))
+  }
 
-    return arr;
+  return arr
 }
 
 export function loadCppModule(): Promise<PortedCppModule> {
-    return new Promise((resolve, reject) => {
-        const s = document.createElement('script')
-        s.src = 'res/strategy.js'
-        s.onload = () => {
-            const cppModule = window.Module
-            cppModule.onRuntimeInitialized = () => {
-                resolve({
-                    calc: (cards, mainRank): StrategyResult => {
-                        const { minHands, solutions: originalSolutions } = cppModule.calc(cards, mainRank.charCodeAt(0))
+  return new Promise((resolve, reject) => {
+    const s = document.createElement('script')
+    s.src = 'res/strategy.js'
+    s.onload = () => {
+      const cppModule = window.Module
+      cppModule.onRuntimeInitialized = () => {
+        resolve({
+          calc: (cards, mainRank): StrategyResult => {
+            const { minHands, solutions: originalSolutions } = cppModule.calc(
+              cards,
+              mainRank.charCodeAt(0),
+            )
 
-                        return {
-                            minHands,
-                            solutions: vector2Array(originalSolutions),
-                        }
-                    }
-                })
+            return {
+              minHands,
+              solutions: vector2Array(originalSolutions),
             }
-            cppModule.onAbort = () => {
-                reject('wasm module aborted during loading')
-            }
-        }
-        s.onerror = () => reject('wasm module failed to load')
-        document.body.appendChild(s)
-    })
+          },
+        })
+      }
+      cppModule.onAbort = () => {
+        reject('wasm module aborted during loading')
+      }
+    }
+    s.onerror = () => reject('wasm module failed to load')
+    document.body.appendChild(s)
+  })
 }
