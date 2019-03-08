c28103ff125b5725c36d5cf2ad6a7dc556e6b018
Bob Gong <gongyuan94@gmail.com>
2019-03-08 19:47:16 +0800

    refactor strategy.cpp to improve code quality

--- strategy.cpp
+++ strategy.cpp
@@ -10,6 +10,7 @@
 
 #endif
 
+#include <cassert>
 #include <cmath>
 #include <list>
 #include <map>
@@ -21,12 +22,77 @@
 using namespace std;
 
 typedef pair<int, char> TCard;
-typedef map<int, multiset<char> > THandCards;
+typedef map<int, multiset<char>> THandCards;
 typedef list<string> TSolutions;
 const int JOKER = 100000;
 const string SUITS = "SHDCA";
+const int INF = 100000;
 
-int count(THandCards hc) {
+/**
+ * calculates min number of hands needed using just hand patterns of 1s, 2s, 3s,
+ *full houses and bombs
+ *
+ ** cnti: cnti means how many card ranks have exactly i cards
+ * so i has valid range [1, 3] for calculating how many hands left
+ ** wildCards: number of wildcards available, belong to range [0, 2] in a valid
+ *game
+ *
+ ** returns min hands needed
+ */
+int calculateMinHandsSimple(
+    int cnt1, int cnt2, int cnt3, int cnt4plus, int wildCards) {
+    if (wildCards == 0) {
+        return cnt1 + max(cnt2, cnt3);
+    } else {
+        int minHandsPossible = INF;
+        if (cnt1 > 0) {
+            // use one wildcard to play a *single* as a *pair*
+            minHandsPossible =
+                min(minHandsPossible,
+                    calculateMinHandsSimple(cnt1 - 1, cnt2 + 1, cnt3, cnt4plus,
+                                            wildCards - 1));
+        }
+        if (cnt2 > 0) {
+            // use one wildcard to play a *pair* as a *triple*
+            minHandsPossible =
+                min(minHandsPossible,
+                    calculateMinHandsSimple(cnt1, cnt2 - 1, cnt3 + 1, cnt4plus,
+                                            wildCards - 2));
+        }
+        if (cnt3 > 0) {
+            // use one wildcard to play a *triple* as a *bomb*
+            minHandsPossible =
+                min(minHandsPossible,
+                    calculateMinHandsSimple(cnt1, cnt2, cnt3 - 1, cnt4plus + 1,
+                                            wildCards - 1));
+        }
+        if (cnt4plus > 0) {
+            // use one wildcard to play a *bomb* as a larger *bomb*
+            minHandsPossible =
+                min(minHandsPossible,
+                    calculateMinHandsSimple(cnt1, cnt2, cnt3, cnt4plus,
+                                            wildCards - 1));
+        }
+        // use one wildcard as a *single*
+        minHandsPossible = min(
+            minHandsPossible, calculateMinHandsSimple(cnt1 + 1, cnt2, cnt3,
+                                                      cnt4plus, wildCards - 1));
+
+        return minHandsPossible;
+    }
+}
+
+/**
+ * calculates min number of hands needed using just hand patterns of 1s, 2s, 3s,
+ *full houses and bombs
+ *
+ ** hc: a full hand of cards definition
+ ** wildCards: number of wildcards available, belong to range [0, 2] in a valid
+ *game
+ *
+ ** returns min hands needed
+ */
+int count(THandCards hc, int wildCards) {
     THandCards::iterator it;
     int cnt[5];
     for (int i = 1; i <= 4; i++) cnt[i] = 0;
@@ -39,15 +105,15 @@ int count(THandCards hc) {
 
     for (it = hc.begin(); it != hc.end(); it++) {
         if (hasJokerBomb && (*it).first >= JOKER) {
-            continue; // skip jokers if it is a bomb
+            continue;  // skip jokers if it is a bomb
         }
         if ((*it).second.size() <= 3)
             cnt[(*it).second.size()]++;
         else
             cnt[4]++;
     }
-    // cout<<"new "<<cnt[1]<<' '<<cnt[2]<<' '<<cnt[3]<<' '<<cnt[4]<<endl;
-    return cnt[3] > cnt[2] ? cnt[1] + cnt[3] : cnt[1] + cnt[2];
+
+    return calculateMinHandsSimple(cnt[1], cnt[2], cnt[3], cnt[4], wildCards);
 }
 
 void AddCard(THandCards& hc, char ch1, char ch2) {
@@ -90,7 +156,7 @@ string CardToStr(int num, char suit) {
     } else if (num == 10) {
         str += "0";
     }
-    
+
     if (num == JOKER + 1) {
         str += "XR";
     } else if (num == JOKER) {
@@ -101,45 +167,47 @@ string CardToStr(int num, char suit) {
     return str;
 }
 
+// rank 14 -> A -> should be found at rank 1
+int getActualRank(int rank) { return rank == 14 ? 1 : rank; }
+
 // Suit: S-Spade C-Club D-Diamond H-Heart A-All
-int ExistShunZi(THandCards& hc, int StartingNumber, int Length, int HandNum,
-                char Suit, THandCards& oneHand) {
-    bool f = true;
+int ExistShunZi(THandCards& hc,
+                int StartingNumber,
+                int Length,
+                int HandNum,
+                char Suit,
+                THandCards& oneHand) {
     oneHand.clear();
     if (Suit == 'A') {
+        bool f = true;
         for (int k = StartingNumber; k < StartingNumber + Length; k++) {
-            int i = k == 14 ? 1 : k;
-            if (hc[i].size() < HandNum) {
+            if (hc[getActualRank(k)].size() < HandNum) {
                 f = false;
                 break;
             }
         }
         if (!f) return false;
         for (int k = StartingNumber; k < StartingNumber + Length; k++) {
-            int i = k == 14 ? 1 : k;
+            int i = getActualRank(k);
             multiset<char>::iterator it;
             int j;
             for (j = 0, it = hc[i].begin(); j < HandNum; j++, it++) {
-                if (it == hc[i].end()) {
-                    // cout << "error 001" << endl;  // debug
-                    return -1;
-                }
+                assert(it != hc[i].end());
                 oneHand[i].insert(*it);
             }
         }
     } else {
+        bool f = true;
         for (int k = StartingNumber; k < StartingNumber + Length; k++) {
-            int i = k == 14 ? 1 : k;
-            if (hc[i].count(Suit) < HandNum) {
+            if (hc[getActualRank(k)].count(Suit) < HandNum) {
                 f = false;
                 break;
             }
         }
         if (!f) return 0;
         for (int k = StartingNumber; k < StartingNumber + Length; k++) {
-            int i = k == 14 ? 1 : k;
             for (int j = 0; j < HandNum; j++) {
-                oneHand[i].insert(Suit);
+                oneHand[getActualRank(k)].insert(Suit);
             }
         }
     }
@@ -187,111 +255,92 @@ string handToStr(THandCards hc) {
     return str;
 }
 
-int check(THandCards& hc, list<string>& ASolution, int CurrentTypePosition,
+int check(THandCards& hc,
+          list<string>& ASolution,
+          int wildCardsLeft,
+          int CurrentTypePosition,
+          int CurrentStartingNumPosition);
+
+void tryExtractOneHand(char NowSuit,
+                       int seriesCount,
+                       int cardCount,
+                       int& TypePosition,
+                       int CurrentTypePosition,
+                       int& CurrentStartingNumPosition,
+                       THandCards& hc,
+                       int wildCardsLeft,
+                       TSolutions& ASolution,
+                       int& min) {
+    TypePosition++;
+    if (CurrentTypePosition > TypePosition)
+        return;
+    else if (CurrentTypePosition < TypePosition)
+        CurrentStartingNumPosition = 0;
+    for (auto it = hc.begin(); it != hc.end(); it++) {
+        int StartingNumber = (*it).first;
+        if (StartingNumber < CurrentStartingNumPosition) continue;
+        THandCards oneHand;
+        int status = ExistShunZi(hc, StartingNumber, seriesCount, cardCount,
+                                 NowSuit, oneHand);
+        if (status > 0) {
+            Chu(hc, oneHand);
+            list<string> tmpSolution;
+            int remainingHands =
+                check(hc, tmpSolution, wildCardsLeft, TypePosition,
+                      StartingNumber) +
+                // straight flush (5 consecutive cards with the same suit) is a
+                // bomb
+                ((NowSuit != 'A' && seriesCount == 5 && cardCount == 1) ? 0
+                                                                        : 1);
+            if (remainingHands <= min) {
+                if (remainingHands < min) ASolution.clear();
+                min = remainingHands;
+                for (TSolutions::iterator it = tmpSolution.begin();
+                     it != tmpSolution.end(); it++) {
+                    (*it) += "| " + handToStr(oneHand) + "|";
+                }
+                ASolution.splice(ASolution.end(), tmpSolution,
+                                 tmpSolution.begin(), tmpSolution.end());
+            }
+            Mo(hc, oneHand);
+        }
+    }
+}
+
+int check(THandCards& hc,
+          list<string>& ASolution,
+          int wildCardsLeft,
+          int CurrentTypePosition,
           int CurrentStartingNumPosition) {
     THandCards::iterator it;
     int TypePosition = 0;
-    int min = count(hc);
+    int min = count(hc, wildCardsLeft);
     ASolution.push_back("| " + handToStr(hc) + "|");
-    for (int tt = 0; tt < 5; tt++)  // shunzi
-    {
-        char NowSuit = SUITS[tt];
-        TypePosition++;
-        if (CurrentTypePosition > TypePosition)
-            continue;
-        else if (CurrentTypePosition < TypePosition)
-            CurrentStartingNumPosition = 0;
-        // else if(CurrentTypePosition==TypePosition)CurrentStartingNum=0;
-        for (it = hc.begin(); it != hc.end(); it++) {
-            int StartingNumber = (*it).first;
-            if (StartingNumber < CurrentStartingNumPosition) continue;
-            THandCards oneHand;
-            int status =
-                ExistShunZi(hc, StartingNumber, 5, 1, NowSuit, oneHand);
-            if (status > 0) {
-                Chu(hc, oneHand);
-                list<string> tmpSolution;
-                int t = check(hc, tmpSolution, TypePosition,
-                              StartingNumber);  //, StartingNumber);
-                if (NowSuit != 'A') t--;
-                if (t + 1 <= min) {
-                    if (t + 1 < min) ASolution.clear();
-                    min = t + 1;
-                    for (TSolutions::iterator it = tmpSolution.begin();
-                         it != tmpSolution.end(); it++) {
-                        (*it) += "| " + handToStr(oneHand) + "|";
-                    }
-                    ASolution.splice(ASolution.end(), tmpSolution,
-                                     tmpSolution.begin(), tmpSolution.end());
-                }
-                Mo(hc, oneHand);
-            }  // else potential_debug
-        }
+    // try flushes -> straight flush
+    for (int tt = 0; tt < 5; tt++) {
+        tryExtractOneHand(SUITS[tt], 5, 1, TypePosition, CurrentTypePosition,
+                          CurrentStartingNumPosition, hc, wildCardsLeft,
+                          ASolution, min);
     }
-    char NowSuit = 'A';
-    TypePosition++;
-    if (CurrentTypePosition <= TypePosition) {
-        if (CurrentTypePosition < TypePosition) CurrentStartingNumPosition = 0;
-        for (it = hc.begin(); it != hc.end(); it++) {
-            int StartingNumber = (*it).first;
-            if (StartingNumber < CurrentStartingNumPosition) continue;
-            THandCards oneHand;
-            int status =
-                ExistShunZi(hc, StartingNumber, 3, 2, NowSuit, oneHand);
-            if (status > 0) {
-                Chu(hc, oneHand);
-                list<string> tmpSolution;
-                int t = check(hc, tmpSolution, TypePosition, StartingNumber);
-                if (t + 1 <= min) {
-                    if (t + 1 < min) ASolution.clear();
-                    min = t + 1;
-                    for (TSolutions::iterator it = tmpSolution.begin();
-                         it != tmpSolution.end(); it++) {
-                        (*it) += "| " + handToStr(oneHand) + "|";
-                    }
-                    ASolution.splice(ASolution.end(), tmpSolution,
-                                     tmpSolution.begin(), tmpSolution.end());
-                }
-                Mo(hc, oneHand);
-            }  // else potential_debug
-        }
-    }
-    NowSuit = 'A';
-    TypePosition++;
-    if (CurrentTypePosition <= TypePosition) {
-        if (CurrentTypePosition < TypePosition) CurrentStartingNumPosition = 0;
-        for (it = hc.begin(); it != hc.end(); it++) {
-            int StartingNumber = (*it).first;
-            if (StartingNumber < CurrentStartingNumPosition) continue;
-            THandCards oneHand;
-            int status =
-                ExistShunZi(hc, StartingNumber, 2, 3, NowSuit, oneHand);
-            if (status > 0) {
-                Chu(hc, oneHand);
-                list<string> tmpSolution;
-                int t = check(hc, tmpSolution, TypePosition, StartingNumber);
-                if (t + 1 <= min) {
-                    if (t + 1 < min) ASolution.clear();
-                    min = t + 1;
-                    for (TSolutions::iterator it = tmpSolution.begin();
-                         it != tmpSolution.end(); it++) {
-                        (*it) += "| " + handToStr(oneHand) + "|";
-                    }
-                    ASolution.splice(ASolution.end(), tmpSolution,
-                                     tmpSolution.begin(), tmpSolution.end());
-                }
-                Mo(hc, oneHand);
-            }  // else potential_debug
-        }
-    }
+    tryExtractOneHand('A', 3, 2, TypePosition, CurrentTypePosition,
+                      CurrentStartingNumPosition, hc, wildCardsLeft, ASolution,
+                      min);
+    tryExtractOneHand('A', 2, 3, TypePosition, CurrentTypePosition,
+                      CurrentStartingNumPosition, hc, wildCardsLeft, ASolution,
+                      min);
     return min;
 }
 
-int TestTrumpCard(THandCards& hc, int n, int& min, TSolutions& solution, int si,
-                  int sj, THandCards& UsedAs) {
+int TestTrumpCard(THandCards& hc,
+                  int n,
+                  int& min,
+                  TSolutions& solution,
+                  int si,
+                  int sj,
+                  THandCards& UsedAs) {
     if (n <= 0) {
         TSolutions ASolution;
-        int t = check(hc, ASolution, 0, 0);
+        int t = check(hc, ASolution, 0, 0, 0);
         if (t <= min) {
             if (t < min) solution.clear();
             min = t;
