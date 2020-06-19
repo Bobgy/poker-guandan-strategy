aaeae8dec4731e64f235f54b722c7631dfe154da
Yuan (Bob) Gong <gongyuan94@gmail.com>
2020-06-19 13:34:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ function vector2Array(vec) {
     return arr;
 }
 
+
 cppModule.onRuntimeInitialized = () => {
     console.log(cppModule.add(2, 3))
     console.log(cppModule.concat('Hello,', ' World'))
